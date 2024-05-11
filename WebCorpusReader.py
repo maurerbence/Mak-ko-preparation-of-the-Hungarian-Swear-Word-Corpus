a56a73b124242e68b7ddbcb4bda7356b36c3fe0a
fagyallo <fagyi@vivald.net>
2024-05-11 20:58:29 +0200

    gigacorpus reader to WebCorpusReader.py and runnable script

--- WebCorpusReader.py
+++ WebCorpusReader.py
@@ -3,6 +3,7 @@ import gzip
 import pandas as pd
 from multiprocessing import Pool, cpu_count, freeze_support
 import glob
+import json
 
 class Parser:
     def parse(self, lines):
@@ -45,3 +46,40 @@ class ConlluFileProcessor:
                 file_result = pool.apply_async(self._process_file, (file_path,))
                 results.append(file_result.get())
         return pd.concat(results, ignore_index=True)
+
+class GigaCorpusParser(Parser):
+    def parse(self, lines):
+        data=[]
+        for line in lines:
+            data.append(list(unpack(json.loads(line))))
+        return pd.DataFrame(data, columns=['id','index', 'domain', 'warc', 'offset', 'length', 'status', 'mime', 'response_date', 'response_content_type', 'text'])
+
+class JsonlFileProcessor:
+    def __init__(self, dir, parser):
+        self.directory = dir
+        self.parser = parser
+
+    def _process_file(self, file):
+        lines = []
+        with gzip.open(file, "rt", encoding="utf-8") as f:
+            for line in f:
+                lines.append(line)
+        return self.parser.parse(lines)
+
+    def process_files(self):
+        files = glob.glob(os.path.join(self.directory, '*.gz'))
+        results = []
+        with Pool(cpu_count()) as pool:
+            for file in files:
+                file_result = pool.apply_async(self._process_file, (file, ))
+                results.append(file_result.get())
+        return pd.concat(results, ignore_index=True)
+
+
+def unpack(data):
+    for k, v in data.items():
+        if isinstance(v, dict):
+            yield from unpack(v)
+        else:
+            yield v
+
